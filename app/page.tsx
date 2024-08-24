d24c82d526c41f97bafe0e7efb9b81f4f1e79b5c
Magaiver <magaivermagalhaes.mm@gmail.com>
2024-08-24 15:46:41 -0300

    feat: add TattoshopItem component

--- app/page.tsx
+++ app/page.tsx
@@ -3,15 +3,23 @@ import Header from "./_components/hearder"
 import { Button } from "./_components/ui/button"
 import { Input } from "./_components/ui/input"
 import Image from "next/image"
+import { Card, CardContent } from "./_components/ui/card"
+import { Badge } from "./_components/ui/badge"
+import { Avatar, AvatarImage } from "./_components/ui/avatar"
+import { db } from "./_lib/prisma"
+import TattoshopItem from "./_components/tattoshop-item"
 
-export default function Home() {
+const Home = async () => {
+  const tattoshops = await db.tattoshop.findMany({})
+  console.log(tattoshops)
+
   return (
     <>
       <Header />
 
       <div className="p-5">
-        <h2 className="text-xl font-bold">Óla, Magaiver!</h2>
-        <p>Quinta Feira, 22 de Agosto</p>
+        <h2 className="text-xl font-bold">Hello, Magaiver!</h2>
+        <p>Friday, 22 August</p>
 
         <div className="mt-6 flex items-center gap-2">
           <Input placeholder="Faça sua Busca..." />
@@ -30,7 +38,46 @@ export default function Home() {
             priority
           />
         </div>
+
+        <h2 className="mb-3 mt-4 text-xs font-bold uppercase text-gray-400">
+          Appointments
+        </h2>
+
+        <Card className="">
+          <CardContent className="flex justify-between p-0">
+            <div className="flex flex-col gap-2 py-5 pl-5">
+              <Badge className="w-fit">Confirmed</Badge>
+              <h3 className="font-semibold">Simple tattoo</h3>
+
+              <div className="flex items-center gap-2">
+                <Avatar className="h-6 w-6">
+                  <AvatarImage src="https://images.unsplash.com/photo-1654338774369-79e915e94ed1?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8ZXN0JUMzJUJBZGlvJTIwdGF0dWFnZW18ZW58MHx8MHx8fDA%3D" />
+                </Avatar>
+
+                <p className="text-sm">Ink Master</p>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center justify-center border-l-2 border-solid px-5">
+              <p className="text-sm">August</p>
+              <p className="text-2xl">05</p>
+              <p className="text-sm">20:24</p>
+            </div>
+          </CardContent>
+        </Card>
+
+        <h2 className="mb-3 mt-4 text-xs font-bold uppercase text-gray-400">
+          Recommended
+        </h2>
+
+        <div className="flex gap-4 overflow-auto [&::-webkit-scrollbar]:hidden">
+          {tattoshops.map((tattoshop) => (
+            <TattoshopItem key={tattoshop.id} tattoshop={tattoshop} />
+          ))}
+        </div>
       </div>
     </>
   )
 }
+
+export default Home
